d7230ebbeba2310ac686e0b09f7d65fd8a5977e4
Kenji Fleming <kwf1999@gmail.com>
2023-03-04 23:40:26 -0500

    added node modules files and updated index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
 const Employee = require('./lib/Employee');
+const render = require('./src/page-template');
 const employees = [];
 
 function newEmployee() {
