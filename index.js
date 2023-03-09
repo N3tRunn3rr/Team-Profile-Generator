ed8f6cb76b32f9b18b4ded4c6f29e6ddf2a9334e
Kenji Fleming <kwf1999@gmail.com>
2023-03-08 21:33:03 -0500

    finally got the test to run. just need to get it to display to html file now

--- index.js
+++ index.js
@@ -3,79 +3,197 @@ const fs = require('fs');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
-const Employee = require('./lib/Employee');
-const render = require('./src/page-template');
+// const generateHTML = require('./src/helper');
+// const Employee = require('./lib/Employee');
 const employees = [];
 
-function newEmployee() {
-    return inquirer.prompt ([
-    {
-        type: 'input',
-        message: 'What is your name?',
-        name: 'name',
-    },
-    {
-        type: 'input',
-        message: 'What is your ID?',
-        name: 'id',
-    },
-    {
-        type: 'input',
-        message: 'What is your email?',
-        name: 'email',
-    },
-    {
-        type: 'list',
-        message: 'What is your role?',
-        role: 'role',
-        choices: ['Engineer', 'Intern', 'Manager'],
-    },
-]).then((answers) => {
-    if ( answers.role === 'Engineer') {
-        inquirer.prompt([{
-            type: 'input',
-            message: 'What is your GitHub username?',
-            name: 'github',
-        }])}
-    else if (answers.role === 'Intern') {
-        inquirer.prompt([{
-            type: 'input',
-            message: 'What school do you attend?',
-            name: 'school',
-        }])}
-    else if (answers.role === 'Manager') {
-        inquirer.prompt([{
-            type: 'input',
-            message: 'What is your office number?',
-            name: 'officeNumber',
-        }])}
-    }).then((answers) => {
-        if (answers.role === 'Engineer') {
-            const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
-            employees.push(engineer);
-        }
-        else if (answers.role === 'Intern') {
-            const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
-            employees.push(intern);
-        }
-        else if (answers.role === 'Manager') {
-            const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
-            employees.push(manager);
-        }
-    })
-}
+//node index.js runs the first question but errors out after you give it a name
 
 function init() {
-    newEmployee()
-    .then((answers) => employees.push(answers))
-    .then(buildTeam)
-    .catch(function(err) {
-    console.log(err)
-    });
+        return newEmployee();
 }
 
-function buildTeam () {
-    fs.writeFileSync('./dist/output.html', render(employees), "utf-8");
+function newEmployee() {
+    inquirer.prompt([
+    {
+        type: 'list',
+        name: 'role',
+        message: 'What is your role?',
+        choices: ['Engineer', 'Intern', 'Manager'],
+    }
+]).then((answer) => {
+    if (answer.role === 'Engineer') { 
+            return inquirer.prompt([
+            {
+            
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is your ID?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is your email?',
+            },
+            {
+            type: 'input',
+            name: 'github',
+            message: 'What is your GitHub username?',
+            },
+        ])
+        .then((data) => {
+        const engineer = new Engineer(data.name, data.id, data.email, data.github);
+        employees.push(engineer);
+        console.log(engineer)
+        });
+    } else if (answer.role === 'Intern') {
+        return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is your ID?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email?',
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What school do you attend?',
+        },
+    ]).then((data) => {
+        const intern = new Intern(data.name, data.id, data.email, data.school);
+        employees.push(intern);
+        console.log(intern)
+        })
+    } else if (answer.role === 'Manager') {
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?',
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: 'What is your ID?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is your email?',
+            },
+            {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is your office number?',
+        }
+    ]).then((data) => {
+        const manager = new Manager(data.name, data.id, data.email, data.officeNumber);
+        employees.push(manager);
+        console.log(manager)
+        })
+    }
+}).then((data) => {
+    // const employee = new Employee(data);
+    // const htmlInfo = generateHTML(data);
+    fs.writeFile('./output.html', generateHTML(data),  (err) =>
+    err ? console.log(err) : console.log('Success!')
+    );
+    employees.push(data);
+    console.log(JSON.stringify(employees));
+})
 }
+
+function generateHTML() {
+    let html = '';
+    if (constructor.name === 'Manager') {
+            html += 
+  `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <title>Employee Profile</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Hi! My name is ${name}</h1>
+    <p class="lead">My work id is: ${id}.</p>
+    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <ul class="list-group">
+      <li class="list-group-item">My email is ${email}</li>
+      <li class="list-group-item">My Office Number Is: ${officeNumber}</li>
+    </ul>
+  </div>
+</div>
+</body>
+</html>`;
+} else if (constructor.role === 'Engineer') {
+    html += `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+      <title>Employee Profile</title>
+    </head>
+    <body>
+      <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Hi! My name is ${name}</h1>
+        <p class="lead">My work id is: ${id}.</p>
+        <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+        <ul class="list-group">
+          <li class="list-group-item">My email is ${email}</li>
+          <li class="list-group-item">My github is: ${github}</li>
+        </ul>
+      </div>
+    </div>
+    </body>
+    </html>`;
+} else if (constructor.role === 'Intern') {
+    html += `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+      <title>Employee Profile</title>
+    </head>
+    <body>
+      <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-4">Hi! My name is ${name}</h1>
+        <p class="lead">My work id is: ${id}.</p>
+        <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+        <ul class="list-group">
+          <li class="list-group-item">My email is ${email}</li>
+          <li class="list-group-item">My School Is: ${school}</li>
+        </ul>
+      </div>
+    </div>
+    </body>
+    </html>`;
+}
+return html;
+}
+
+
+
 
 init();
---
+++ src/helper.js
@@ -0,0 +1,82 @@
+
+
+// function generateHTML() {
+//     let html = '';
+//     if (role === 'Manager') {
+//             html += 
+//   `<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+//   <title>Employee Profile</title>
+// </head>
+// <body>
+//   <div class="jumbotron jumbotron-fluid">
+//   <div class="container">
+//     <h1 class="display-4">Hi! My name is ${name}</h1>
+//     <p class="lead">My work id is: ${id}.</p>
+//     <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+//     <ul class="list-group">
+//       <li class="list-group-item">My email is ${email}</li>
+//       <li class="list-group-item">My Office Number Is: ${officeNumber}</li>
+//     </ul>
+//   </div>
+// </div>
+// </body>
+// </html>`;
+// } else if (role === 'Engineer') {
+//     html += `<!DOCTYPE html>
+//     <html lang="en">
+//     <head>
+//       <meta charset="UTF-8">
+//       <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+//       <title>Employee Profile</title>
+//     </head>
+//     <body>
+//       <div class="jumbotron jumbotron-fluid">
+//       <div class="container">
+//         <h1 class="display-4">Hi! My name is ${name}</h1>
+//         <p class="lead">My work id is: ${id}.</p>
+//         <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+//         <ul class="list-group">
+//           <li class="list-group-item">My email is ${email}</li>
+//           <li class="list-group-item">My github is: ${github}</li>
+//         </ul>
+//       </div>
+//     </div>
+//     </body>
+//     </html>`;
+// } else if (role === 'Intern') {
+//     html += `<!DOCTYPE html>
+//     <html lang="en">
+//     <head>
+//       <meta charset="UTF-8">
+//       <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+//       <title>Employee Profile</title>
+//     </head>
+//     <body>
+//       <div class="jumbotron jumbotron-fluid">
+//       <div class="container">
+//         <h1 class="display-4">Hi! My name is ${name}</h1>
+//         <p class="lead">My work id is: ${id}.</p>
+//         <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+//         <ul class="list-group">
+//           <li class="list-group-item">My email is ${email}</li>
+//           <li class="list-group-item">My School Is: ${school}</li>
+//         </ul>
+//       </div>
+//     </div>
+//     </body>
+//     </html>`;
+// }
+// return html;
+// }
+
+
+
+
+// module.exports = generateHTML;
